781fd5de5327c8d9e1707acec6a61afafe9b81e8
captain <aP7UUAN>
2021-09-15 22:49:04 +0300

    rename weth to wvlx

--- src/weth9.sol
+++ src/weth9.sol
@@ -16,8 +16,8 @@
 pragma solidity >=0.4.23;
 
 contract WETH9_ {
-    string public name     = "Wrapped Ether";
-    string public symbol   = "WETH";
+    string public name     = "Wrapped VLX";
+    string public symbol   = "WVLX";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
